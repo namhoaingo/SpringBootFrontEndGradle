155c973c28c1ec01142cffaef3c41dce027309c7
Your Name <namhoaingo@melaleuca.com>
2017-09-19 18:02:39 +0800

    Thymeleaf

--- src/main/java/com/example/springbootfrontendgradle/MvcControllers/HomeController.java
+++ src/main/java/com/example/springbootfrontendgradle/MvcControllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.springbootfrontendgradle.MvcControllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String Index(){
+    public String Index(Model model){
+        model.addAttribute("name", "Name");
         return "main";
     }
 }
